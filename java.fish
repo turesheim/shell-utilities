121b7982b50f0fcccc3f1fbcbc7dd2bbad5a5b3f
Torkild U. Resheim <torkildr@gmail.com>
2017-06-05 11:57:04 +0200

    Add support for Java 9

--- java.fish
+++ java.fish
@@ -6,9 +6,10 @@ alias jjs='rlwrap /Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home/b
 alias j6='ju 1.6'
 alias j7='ju 1.7'
 alias j8='ju 1.8'
+alias j9='ju 1.9'
 
 function jls
-	echo "Use ´j6, j7 and j8´ to conventiently switch between Java versions."
+	echo "Use ´j6, j7, j8 and j9´ to conventiently switch between Java versions."
 	echo "For a specific version use ´ju <version>´ where available versions are listed below:"
 	/usr/libexec/java_home -V 2>&1 | grep -E "\d.\d.\d" | cut -d , -f 1 | colrm 1 4 | grep -v Home
 end
@@ -19,6 +20,5 @@ function ju
 	java -version
 end
 
-set JAVA_HOME (/usr/libexec/java_home -v 1.7)
+set JAVA_HOME (/usr/libexec/java_home -v 1.8)
 set PATH $JAVA_HOME/bin $PATH
-            
--- java.zsh
+++ java.zsh
@@ -12,7 +12,7 @@ alias j9='ju 1.9'
 alias jh=/usr/libexec/java_home
 
 function jls() {
-	echo "Use ´j6, j7 and j8´ to conventiently switch between Java versions."
+	echo "Use ´j6, j7, j8 and j9´ to conventiently switch between Java versions."
 	echo "For a specific version use ´ju <version>´ where available versions are listed below:"
 	/usr/libexec/java_home -V 2>&1 | grep -E "\d(.\d.\d)?" | cut -d , -f 1 | grep -v Home
 }
@@ -26,4 +26,3 @@ function ju() {
 
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 export PATH=$JAVA_HOME/bin:$PATH
-            
